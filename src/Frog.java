4b49e97dd6c927fe3a73c05118d5bf3042b2d32a
Etienne Lafond <et998726@dal.ca>
2022-12-16 10:18:18 -0400

    Created required methods in Frog.java

--- src/Frog.java
+++ src/Frog.java
@@ -1,4 +1,24 @@
 import java.awt.*;
+import java.awt.event.KeyEvent;
 
 public class Frog extends Rectangle {
+    Frog() {
+
+    }
+
+    public void move() {
+
+    }
+
+    public void draw() {
+        
+    }
+
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    public void keyReleased(KeyEvent e) {
+
+    }
 }

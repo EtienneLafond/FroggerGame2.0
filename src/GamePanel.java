33a68270737d1cf1be424671e79510ca4dd88376
Etienne Lafond <et998726@dal.ca>
2022-12-16 10:15:34 -0400

    Created all required methods in GamePanel.java

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,4 +1,70 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
+
+    // Game dimensions
+    static final int GAME_WIDTH = 1000;
+    static final int GAME_HEIGHT = 600;
+    static final Dimension SCREEN_SIZE = new Dimension(GAME_WIDTH, GAME_HEIGHT);
+
+    // Frog (player) dimensions
+    static final int FROG_WIDTH = 50;
+    static final int FROG_HEIGHT = 50;
+
+    // Game components
+    Thread gameThread;
+    Image image;
+    Graphics graphics;
+    Frog frog;
+    Car car1;
+    Car car2;
+    Car car3;
+
+    /**
+     * Constructor
+     * Creates the frog and cars, starts the threat and set panel size.
+     */
+    GamePanel() {
+
+    }
+
+    public void newFrog() {
+
+    }
+
+    public void newCar() {
+
+    }
+
+    public void paint(Graphics g) {
+
+    }
+
+    public void move() {
+
+    }
+
+    public void checkCollision() {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    public class AL extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+
+        }
+    }
 }

3df228581fffd7c12299fcd30b2e73a72a3ccb44
Etienne Lafond <et998726@dal.ca>
2022-12-16 10:27:46 -0400

    Created variables and implemented constructor in Frog.java

--- src/Frog.java
+++ src/Frog.java
@@ -2,8 +2,18 @@ import java.awt.*;
 import java.awt.event.KeyEvent;
 
 public class Frog extends Rectangle {
-    Frog() {
+    // Starting position (figure out how to use later)
+    final int xStart = GamePanel.GAME_WIDTH/2;
+    final int yStart = GamePanel.GAME_HEIGHT-100;
 
+    // Variables
+    String name;
+    int xVelocity;
+    int yVelocity;
+    int speed;
+
+    Frog(int width, int height) {
+        super(500, 500, width, height);
     }
 
     public void move() {
